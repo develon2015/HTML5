c50256bc812f8182f4ee055ee95ff8d287f39d04
develon2015 <302615249@qq.com>
2021-01-14 18:20:11 +0800

    add @material-ui/core

---
+++ src/App/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Button } from '@material-ui/core';
+
+export default class extends React.Component {
+    render() {
+        return (
+            <>
+                <Button variant="contained" color="primary">
+                    Hello
+                </Button>
+            </>
+        );
+    }
+};
